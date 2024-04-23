d7c21fe35bf270089f4b568cb8c8d91313c94106
Supafei <fetra.rabehasy@gmail.com>
2024-04-23 14:36:20 +0200

    UPDATE Navbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
 import alpine from '@astrojs/alpinejs';
+import tailwind from '@astrojs/tailwind'
 
 export default defineConfig({
-  integrations: [alpine()],
+  integrations: [alpine(),tailwind()],
 });
 
---
+++ src/pages/index.md
@@ -0,0 +1,9 @@
+---
+title: index
+layout: "../layouts/Layout.astro"
+---
+
+# Hello world 
+
+## Section 1
+## Section 2
